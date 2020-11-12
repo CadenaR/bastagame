791db919a5d8f763752c5461421a8c831254bd7a
CadenaR <a01652141@itesm.mx>
2020-11-12 03:07:55 -0600

    [UPDATE] Game starts

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,19 @@
-function makeToast() {
-  $.toast({
-    text: 'This is called a toast!',
-    position: 'top-right'
-  });
-}
+let waitingRoom = $("#waitingRoom");
+let playerName = $("#playerName");
+let startBtn = $("#startBtn");
+let gameRoom = $("#gameRoom");
+let letter = $("#letter");
+let name = $("#name");
+let color = $("#color");
+let fruit = $("#fruit");
+let podium = $("#podium");
+let winner = $("#winner");
+let delivered = false;
 
 function makeToastMessage(message) {
   $.toast({
     text: message,
-    position: 'top-right'
+    position: "top-center",
   });
 }
 
@@ -17,18 +22,118 @@ function connectToSocketIo() {
   let server = window.location.protocol + "//" + window.location.host;
   window.socket = io.connect(server);
   // Recibe un mensaje de tipo toast
-  window.socket.on('toast', function (data) {
+  window.socket.on("toast", function (data) {
     // Muestra el mensaje
     makeToastMessage(data.message);
   });
+
+  window.socket.on("init", function (data) {
+    setName(data.playerName);
+  });
+
+  window.socket.on("start:button", function () {
+    activateStartButton();
+  });
+
+  window.socket.on("button:disable", function () {
+    disable();
+  });
+
+  window.socket.on("button:hide", function () {
+    hideStartBtn();
+  });
+
+  window.socket.on("start:game", function () {
+    switchToGame();
+  });
+
+  window.socket.on("letter", function (data) {
+    changeLetter(data.character);
+  });
+
+  window.socket.on("game:stop", function () {
+    stopGame();
+  });
+
+  window.socket.on("game:stop", function (data) {
+    setWinner(data.winner);
+  });
+
+  window.socket.on("finish", function () {
+    exitRoom()
+  });
 }
 
-function emitEventToSocketIo() {
-  let text = $('#messageToServer').val();
-  // Envía un mensaje
-  window.socket.emit('messageToServer', { text: text });
+function setName(pName) {
+  playerName.text(`Hello ${pName}`);
+}
+
+function activateStartButton() {
+  startBtn.show();
+}
+
+function hideStartBtn() {
+  startBtn.hide();
+}
+
+function startGame() {
+  startBtn.prop("disabled", true);
+  window.socket.emit("server:start:game");
+}
+
+function disable() {
+  startBtn.prop("disabled", true);
+}
+
+function switchToGame() {
+  window.socket.emit("joinMe");
+  startBtn.prop("disabled", false);
+  startBtn.hide();
+  waitingRoom.hide();
+  gameRoom.show();
+}
+
+function changeLetter(chr) {
+  letter.text(chr);
+}
+
+function submitAnswers() {
+  if (name.val() && color.val() && fruit.val()) {
+    let ans = { name: name.val(), color: color.val(), fruit: fruit.val() };
+    delivered = true;
+    window.socket.emit("server:basta", { ans: ans });
+  } else {
+    makeToastMessage("Fill all the fields");
+  }
+}
+
+function stopGame() {
+  if (!delived) {
+    let ans = { name: name.val(), color: color.val(), fruit: fruit.val() };
+
+    window.socket.emit("server:basta", { ans: ans });
+  }
+}
+
+function setWinner(gameWinner) {
+  podium.show();
+  gameRoom.hide();
+  winner.text(gameWinner);
+}
+
+function endGame() {
+  window.socket.emit("server:exit");
+}
+
+function exitRoom() {
+  window.socket.emit("kickMe");
+  waitingRoom.show();
+  gameRoom.hide()
+  podium.hide();
 }
 
 $(function () {
   connectToSocketIo();
+  gameRoom.hide();
+  podium.hide();
 });
--- server.js
+++ server.js
@@ -1,47 +1,50 @@
 // Imports
-const express = require('express');
-const webRoutes = require('./routes/web');
+const express = require("express");
+const webRoutes = require("./routes/web");
 
 // Session imports
-let cookieParser = require('cookie-parser');
-let session = require('express-session');
-let flash = require('express-flash');
-let passport = require('passport');
+let cookieParser = require("cookie-parser");
+let session = require("express-session");
+let flash = require("express-flash");
+let passport = require("passport");
 
 // Express app creation
 const app = express();
-const server = require('http').Server(app);
-const io = require('socket.io')(server);
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 // Configurations
-const appConfig = require('./configs/app');
+const appConfig = require("./configs/app");
 
 // View engine configs
-const exphbs = require('express-handlebars');
+const exphbs = require("express-handlebars");
 const hbshelpers = require("handlebars-helpers");
+const { clearInterval } = require("timers");
 const multihelpers = hbshelpers();
-const extNameHbs = 'hbs';
+const extNameHbs = "hbs";
 const hbs = exphbs.create({
   extname: extNameHbs,
-  helpers: multihelpers
+  helpers: multihelpers,
 });
 app.engine(extNameHbs, hbs.engine);
-app.set('view engine', extNameHbs);
+app.set("view engine", extNameHbs);
 
 // Session configurations
-let sessionStore = new session.MemoryStore;
+let sessionStore = new session.MemoryStore();
 app.use(cookieParser());
-app.use(session({
-  cookie: { maxAge: 60000 },
-  store: sessionStore,
-  saveUninitialized: true,
-  resave: 'true',
-  secret: appConfig.secret
-}));
+app.use(
+  session({
+    cookie: { maxAge: 60000 },
+    store: sessionStore,
+    saveUninitialized: true,
+    resave: "true",
+    secret: appConfig.secret,
+  })
+);
 app.use(flash());
 
 // Passport configurations
-require('./configs/passport');
+require("./configs/passport");
 app.use(passport.initialize());
 app.use(passport.session());
 
@@ -49,27 +52,111 @@ app.use(passport.session());
 app.use(express.urlencoded({ extended: true }));
 
 // Route for static files
-app.use('/', express.static(__dirname + '/public'));
+app.use("/", express.static(__dirname + "/public"));
 
 // Routes
-app.use('/', webRoutes);
+app.use("/", webRoutes);
 
-io.on('connection', (socket) => {
+/**
+ * Websocket handling
+ */
+
+playerHistoryCount = 0;
+players = {};
+roomPlayers = {};
+inGame = false;
+
+io.on("connection", (socket) => {
   // Recibe la conexión del cliente
-  console.log('Client connected...');
-  let i = 0;
-  // Emite un mensaje
-  setInterval(() => {
-      socket.emit('toast', { message: `Message: ${i}`});
-      i++;
-  }, 3000);
+  console.log("Client " + socket.id + " connected...");
+
+  playerHistoryCount++;
+  players[socket.id] = {
+    pName: playerHistoryCount,
+    name: "",
+    color: "",
+    fruit: "",
+    score: 0
+  };
+
+  socket.emit("toast", { message: `Hi player ${players[socket.id].pName}` });
+  socket.broadcast.emit("toast", {
+    message: `Player ${players[socket.id].pName} has joined`,
+  });
+
+  socket.emit("init", {
+    playerName: `Player ${players[socket.id].pName}`,
+  });
+
+  if (Object.keys(roomPlayers).length == 0) {
+    if (Object.keys(players).length > 1) {
+      io.sockets.emit("start:button");
+    }
+  }
+
+  socket.on("server:start:game", () => {
+    io.sockets.emit("button:disable");
+
+    let i = 3;
+    var timer = setInterval(() => {
+      io.sockets.emit("toast", { message: i });
+      i--;
+      if (i == 0) clearInterval(timer);
+    }, 1000);
+
+    setTimeout(function () {
+      io.sockets.emit("start:game");
+      roomPlayers = players;
+      players = {};
+    }, 4000);
+  });
+
+  socket.on("joinMe", () => {
+    socket.join("gameRoom");
+  });
+
+  socket.on("kickMe", () => {
+    socket.leave("gameRoom");
+    players[socket.id] = {
+      pName: roomPlayers[socket.id].pName,
+      name: "",
+      color: "",
+      fruit: "",
+      score: 0
+    };
+    delete roomPlayers[socket.id];
+  });
+
   // Recibe un mensaje
-  socket.on('messageToServer', (data) => {
-      console.log('messageReceivedFromClient: ', data.text);
+  socket.on("messageToServer", (data) => {
+    console.log("messageReceivedFromClient: ", data.text);
+  });
+
+  socket.on("disconnect", () => {
+    if (socket.id in players) {
+      socket.broadcast.emit("toast", {
+        message: `Player ${players[socket.id]["pName"]} has left`,
+      });
+      delete players[socket.id];
+      if (Object.keys(players).length == 1) {
+        io.sockets.emit("button:hide")
+      }
+    } else {
+      socket.broadcast.emit("toast", {
+        message: `Player ${roomPlayers[socket.id]["pName"]} has left`,
+      });
+      delete roomPlayers[socket.id];
+      if (Object.keys(roomPlayers).length == 1) {
+        io.to("gameRoom").emit("toast", { message: 'No players left' });
+        io.to("gameRoom").emit("finish");
+      }
+    }
   });
 });
 
 // App init
 server.listen(appConfig.expressPort, () => {
-  console.log(`Server is listenning on ${appConfig.expressPort}! (http://localhost:${appConfig.expressPort})`);
+  console.log(
+    `Server is listenning on ${appConfig.expressPort}! (http://localhost:${appConfig.expressPort})`
+  );
 });
